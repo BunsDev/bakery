743d8e6842f97ea61f120964cbf11f9a272e5be6
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-11-21 17:51:49 -0600

    Update Logo.tsx
    32px

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -6,7 +6,7 @@ import logo from '../../assets/img/cuplogo.png'
 const Logo: React.FC = () => {
   return (
     <StyledLogo to="/">
-      <img src={logo} height="72" style={{ marginTop: -4 }} />
+      <img src={logo} height="32" style={{ marginTop: -4 }} />
       <StyledText>
         GOVDAO Bakery
       </StyledText>
