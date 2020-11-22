595a547e13d9e336269672ec1b852864f33c45ed
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-11-21 18:07:06 -0600

    Update AccountButton.tsx
    Unlock wallet set to sm for mobile view.

--- src/components/TopBar/components/AccountButton.tsx
+++ src/components/TopBar/components/AccountButton.tsx
@@ -24,9 +24,9 @@ const AccountButton: React.FC<AccountButtonProps> = (props) => {
   return (
     <StyledAccountButton>
       {!account ? (
-        <Button onClick={handleUnlockClick} size="md" text="Unlock Wallet" />
+        <Button onClick={handleUnlockClick} size="sm" text="Unlock Wallet" />
       ) : (
-        <Button onClick={onPresentAccountModal} size="md" text="My Wallet" />
+        <Button onClick={onPresentAccountModal} size="sm" text="My Wallet" />
       )}
     </StyledAccountButton>
   )
