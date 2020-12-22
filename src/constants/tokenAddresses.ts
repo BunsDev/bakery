826bb41a56c80087a17d77753ad5690771b06149
uni <68980965+CryptoUnico@users.noreply.github.com>
2020-12-21 19:36:48 -0600

    Update tokenAddresses.ts

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,5 +1,6 @@
 export const gdao = '0x060dea069f4a0cf3f359152ec02f048ce9930686'
 export const gdaov2 = '0xb354b410071a12b5ccb28bd3275a44c6dc9dbc61'
 export const gdaoAddress = '0x060dea069f4a0cf3f359152ec02f048ce9930686'
-export const masterChefAddress = '0xdf2A82d8a34F139f649000e43f60C921F769a947'
+export const masterChefAddress = '0x3a6cfFAea224483657E9F483d174b365A27AAA78'
+//export const masterChefAddress = '0xdf2A82d8a34F139f649000e43f60C921F769a947'
 export const weth = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
